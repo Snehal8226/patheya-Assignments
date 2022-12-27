f42dddee84d3ccf3b502839b83d884d6356c059f
Snehal <117708032+Snehal8226@users.noreply.github.com>
2022-12-27 17:33:14 +0530

    app.js updated

--- ReactJs/my-app/src/App.js
+++ ReactJs/my-app/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+
+  const name = 'Agastya Shinde';
+const element = <h1>Hello, {name}</h1>;
+
+function greet(name1) {
+
+       return <h3>Welcome {name1}</h3>
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h1>Hiiiiiii....This is my first ReactJs app!!!</h1>
-        </a>
-      </header>
+    <div>
+      <h1>new project</h1>
+      <h2> {element} </h2>
+      {greet("ritesh")}
+    
     </div>
   );
 }
